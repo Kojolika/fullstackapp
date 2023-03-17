618e7f377db1338d4409812efdca8388a979c9a2
Andrew Myshok <amm982@live.com>
2023-03-17 10:20:56 -0400

    Added react-select package

---
+++ frontend/src/Pages/AddLocation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AddLocation = () => {
+  return (
+    <div>
+      <button>Add my location</button>
+      <div></div>
+    </div>
+  )
+}
+
+export default AddLocation
