e8f36adcd8b39a2c481e3074da2ba23ef59714a5
Andrew Myshok <amm982@live.com>
2023-04-13 12:42:24 -0400

    Added temperature object that follows appl state

---
+++ frontend/src/Components/Atoms/Temperature.js
@@ -0,0 +1,18 @@
+import { useSelector } from "react-redux";
+import { selectTempUnit } from "../../Features/user_preferences/preferenceSlice";
+
+const Temperature = (props) => {
+    const { temperature, unit } = props.options;
+    const currentTempUnit = useSelector(selectTempUnit);
+
+    const convertToF = (temp) => Math.round((temp * 1.8) + 32);
+
+    const convertToC = (temp) => Math.round((temp - 32) * (5 / 9));
+
+    const finalDisplay = unit === currentTempUnit ? temperature 
+    : currentTempUnit === "Celcius" ? convertToC(temperature) : convertToF(temperature);
+
+    return finalDisplay;
+}
+
+export default Temperature
--- frontend/src/Components/Pages/LocationData.js
+++ frontend/src/Components/Pages/LocationData.js
@@ -3,32 +3,34 @@ import { useState } from "react";
 
 import { selectCurrentLocation } from "../../Features/locations/locationsSlice"
 import { useGetCityWeatherDataQuery } from "../../Features/locations/airVisualApiSlice";
-import { useGetDailyForecast5DaysQuery, useGetDailyForecastQuery, useGetHourlyForecast12HoursQuery, useGetLocationKeyQuery } from "../../Features/locations/accuWeatherApiSlice";
+import { useGetDailyForecast5DaysQuery, useGetHourlyForecast12HoursQuery, useGetLocationKeyQuery } from "../../Features/locations/accuWeatherApiSlice";
 
 import '../../Styles/location.css';
 import '../../Styles/app.css';
 
 import { StarBorder, StarFilled } from "../../Icons/svgImages/index.js";
 import { selectCurrentUser } from "../../Features/auth/authSlice";
+import { selectTempUnit } from "../../Features/user_preferences/preferenceSlice";
+
+import Temperature from "../Atoms/Temperature";
 
 const LocationData = () => {
     const [isFavorited, setIsFavorited] = useState(false);
 
     const user = useSelector(selectCurrentUser);
-
     const locationBeingDisplayed = useSelector(selectCurrentLocation);
 
     //this is for Air Visual API, you need to query it as 'USA' not 'United States'
-    const locationBeingDisplayAirVisualCall = locationBeingDisplayed?.country?.name === 'United States' ? 
-    {
-        "city" : locationBeingDisplayed.city,
-        "province": locationBeingDisplayed.province,
-        "country" : {
-            "name" : "USA",
-            "iso2" : locationBeingDisplayed.country.iso2
-        }
-    } :locationBeingDisplayed;
-    
+    const locationBeingDisplayAirVisualCall = locationBeingDisplayed?.country?.name === 'United States' ?
+        {
+            "city": locationBeingDisplayed.city,
+            "province": locationBeingDisplayed.province,
+            "country": {
+                "name": "USA",
+                "iso2": locationBeingDisplayed.country.iso2
+            }
+        } : locationBeingDisplayed;
+
     const skip = locationBeingDisplayed.city === null ? true : false;
 
     const { currentData: currentDataAirVisual,
@@ -38,15 +40,13 @@ const LocationData = () => {
         isError: isErrorAirVisual,
         isFetching: isFetchingAirVisual } = useGetCityWeatherDataQuery(locationBeingDisplayAirVisualCall, { skip })
 
-/*     const { currentData: locationKey } = useGetLocationKeyQuery(locationBeingDisplayed, { skip });
+    const { currentData: locationKey } = useGetLocationKeyQuery(locationBeingDisplayed, { skip });
     console.log(locationKey);
     const skipForecasts = locationKey === undefined ? true : false;
-    const {currentData: currentDataDailyForecast } = useGetDailyForecastQuery(locationKey, {skip: skipForecasts});
-    const {currentData: currentDataDailyForecast5Days} = useGetDailyForecast5DaysQuery(locationKey, {skip: skipForecasts});
-    const {currentData: currentDataHourlyForecast12Hours} = useGetHourlyForecast12HoursQuery(locationKey, {skip: skipForecasts});
-    console.log(currentDataDailyForecast);
+    const { currentData: currentDataDailyForecast5Days } = useGetDailyForecast5DaysQuery(locationKey, { skip: skipForecasts });
+    const { currentData: currentDataHourlyForecast12Hours } = useGetHourlyForecast12HoursQuery(locationKey, { skip: skipForecasts });
     console.log(currentDataDailyForecast5Days);
-    console.log(currentDataHourlyForecast12Hours); */
+    console.log(currentDataHourlyForecast12Hours);
 
     const locationName = locationBeingDisplayed.city === null ? <></> : <div id='location-name'>
         {locationBeingDisplayed.city.name}, {locationBeingDisplayed.province.name}, {locationBeingDisplayed.country.name}
@@ -55,11 +55,18 @@ const LocationData = () => {
     const dateFormatted = isSuccessAirVisual ? currentDataAirVisual?.data?.current?.weather?.ts.slice(5, 10) : <></>
     const timeFormatted = isSuccessAirVisual ? currentDataAirVisual?.data?.current?.weather?.ts.slice(11, 16) : <></>
 
+    const temperatureUnit = useSelector(selectTempUnit);
+    const tempDegreeLetter = temperatureUnit === "Celcius" ? 'C' : 'F' ;
+
+    const currentTemperature = isSuccessAirVisual ?
+        <span><Temperature options={{ temperature: currentDataAirVisual?.data?.current?.weather?.tp, unit: "Celcius" }} />° {tempDegreeLetter} </span>
+        : <></>
+
     const toggleFavorite = () => {
         if (isFavorited) setIsFavorited(false);
         else setIsFavorited(true);
     }
-    const addLocationToUserDatabase = () =>{
+    const addLocationToUserDatabase = () => {
 
     }
 
@@ -70,7 +77,7 @@ const LocationData = () => {
         {locationName}
         <div id="location-data" >
             <div id='weather' className="weather-panel border-no-shadow">
-                <div id='temperature'>{currentDataAirVisual?.data?.current?.weather?.tp}°C</div>
+                <div id='temperature'>{currentTemperature}</div>
                 <label htmlFor="temperature" id='label'>Current Weather</label>
             </div>
             <div id='pollution' className="weather-panel border-no-shadow">
@@ -79,11 +86,11 @@ const LocationData = () => {
             </div>
         </div>
         <div id='other-weather-data' className="weather-panel border-no-shadow">
-                    <span className="other-weather-data-entry" id="humidity">{currentDataAirVisual?.data?.current?.weather?.hu}%</span>
-                    <span className="other-weather-data-entry" id="wind-speed">{currentDataAirVisual?.data?.current?.weather?.ws} m/s</span>
-                    <span className="other-weather-data-entry" id='wind-direction'>{currentDataAirVisual?.data?.current?.weather?.wd}°</span>
-                    <span className="other-weather-data-entry" id='atmospheric-pressure'>{currentDataAirVisual?.data?.current?.weather?.pr} hPa</span>
-                </div>
+            <span className="other-weather-data-entry" id="humidity">{currentDataAirVisual?.data?.current?.weather?.hu}%</span>
+            <span className="other-weather-data-entry" id="wind-speed">{currentDataAirVisual?.data?.current?.weather?.ws} m/s</span>
+            <span className="other-weather-data-entry" id='wind-direction'>{currentDataAirVisual?.data?.current?.weather?.wd}°</span>
+            <span className="other-weather-data-entry" id='atmospheric-pressure'>{currentDataAirVisual?.data?.current?.weather?.pr} hPa</span>
+        </div>
         <div id='hourly-forecast' className="weather-panel border-no-shadow">
             Hourly Forecast Here
         </div>
